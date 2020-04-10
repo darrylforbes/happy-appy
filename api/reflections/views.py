46e1f84ae58d9d0844cf85d95feab2e7ea709d0d
Darryl Forbes <darrylforbes2022@u.northwestern.edu>
2020-04-09 20:07:08 -0500

    Added IsUser permissions for reflections

--- api/reflections/views.py
+++ api/reflections/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, permissions
 from .serializers import ReflectionSerializer
 from .models import Reflection
+from .permissions import IsUser
 
 
 class ReflectionViewSet(viewsets.ModelViewSet):
@@ -8,7 +9,13 @@ class ReflectionViewSet(viewsets.ModelViewSet):
 
     queryset = Reflection.objects.all().order_by("author")
     serializer_class = ReflectionSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.action == "list":
+            permission_classes = [permissions.IsAdminUser]
+        else:
+            permission_classes = [IsUser]
+        return [permission() for permission in permission_classes]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
