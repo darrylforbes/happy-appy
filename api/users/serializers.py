e1328a852aefe96b1f0053776732fa8abf6d20f3
Darryl Forbes <darrylforbes2022@u.northwestern.edu>
2020-06-14 21:20:39 -0500

    Overrode create method in User serializer; specified more user fields

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -14,4 +14,17 @@ class UserSerializer(serializers.ModelSerializer):
         """Meta class to map serializer's fields with the model fields."""
 
         model = User
-        fields = ("id", "username", "reflections")
+        fields = ("id", "username", "reflections", "email", "password")
+        write_only_fields = ("password",)
+        read_only_fields = ("id",)
+
+    def create(self, validated_data):
+        """Creates new user."""
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            password=validated_data["password"],
+            email=validated_data["email"],
+        )
+        user.save()
+
+        return user
